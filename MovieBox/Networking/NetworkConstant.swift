b25e519bf521822aa89c07b91e31b1778c58e9b8
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-08-18 04:17:08 +0630

    Upload share API

---
+++ MovieBox/Networking/NetworkConstant.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkConstants.swift
+//  MovieBox
+//
+//  Created by Aung Kyaw Phyo on 18/08/2024.
+//
+
+import Foundation
+
+class NetworkConstant {
+    
+    public static var shared: NetworkConstant = NetworkConstant()
+    
+    private init() {
+        
+    }
+    
+    public var apiKey: String {
+        get {
+            // API Key from https://api.themoviedb.org
+            return "949d7b353dd77ddbba10faa299a9fd48"
+        }
+    }
+    
+    public var severAddress: String {
+        get {
+            return "https://api.themoviedb.org/3/"
+        }
+    }
+    
+    public var imageSeverAddress: String {
+        get {
+            return "https://image.tmdb.org/t/p/w500/"
+        }
+    }
+    
+}
