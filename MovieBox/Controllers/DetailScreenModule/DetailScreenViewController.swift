4a09a0329e3a9f58f013efb60d5b09d760076538
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-08-22 02:15:24 +0630

    Upload details

---
+++ MovieBox/Controllers/DetailScreenModule/DetailScreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailScreenViewController.swift
+//  MovieBox
+//
+//  Created by Aung Kyaw Phyo on 22/08/2024.
+//
+
+import UIKit
+
+class DetailScreenViewController: UIViewController {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var posterTitleLabel: UILabel!
+    @IBOutlet weak var posterdescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
